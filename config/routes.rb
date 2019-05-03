60842f16458dab7c0252ae9a8753f16468d56a50
LL <limwenyaolester@hotmail.com>
2019-05-03 17:12:34 +0800

    path added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'users#index'
+
+    get '/posts/new' => 'posts#new', as: 'new_post'
+
   resources :users, :posts, :friends, :likes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

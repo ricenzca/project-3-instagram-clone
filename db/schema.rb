40bc92bf51890cdc1ddaebd7fd6a2b95b0ec4d3a
andrewtan2903 <andrewtan2903@hotmail.com>
2019-05-08 12:17:29 +0800

    added admin user, admin can delete users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_071836) do
+ActiveRecord::Schema.define(version: 2019_05_08_012943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_071836) do
     t.text "bio"
     t.integer "phone"
     t.string "gender"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

8828682de96aab96c4ac200c1b069a310bd88580
LL <limwenyaolester@hotmail.com>
2019-05-10 10:07:53 +0800

    komments database set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_012943) do
+ActiveRecord::Schema.define(version: 2019_05_10_020553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "komments", force: :cascade do |t|
+    t.string "komment"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_komments_on_post_id"
+    t.index ["user_id"], name: "index_komments_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "post_id"
@@ -63,6 +73,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_012943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "komments", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
 end

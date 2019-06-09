8d15ee31ea9e92d58be83217f8c09b1fdc20cfdb
ricenzca <ricenzca@hotmail.com>
2019-06-09 21:35:38 +0800

    gitignore fix

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'dvaat1mxu'
-  config.api_key = '861722865622197'
-  config.api_secret = 'P2AjJV7lcBObzUDvrCog_jkVXHs'
-  config.secure = true
-  config.cdn_subdomain = true
-end
